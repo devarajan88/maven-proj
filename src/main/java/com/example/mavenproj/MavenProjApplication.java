9610ed58cfa3c490ad7dc86c68ee290ccb4d8490
Devarajan <devarajangct@gmail.com>
2022-09-22 10:53:15 +0530

    initial commit

---
+++ src/main/java/com/example/mavenproj/MavenProjApplication.java
@@ -0,0 +1,13 @@
+package com.example.mavenproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenProjApplication.class, args);
+	}
+
+}
